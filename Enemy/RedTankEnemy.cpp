41542c9846b538719992d40eaf891197d271b838
9zxu <106156444+9zxu@users.noreply.github.com>
2025-05-19 21:21:23 +0800

    Red Tank enemy added

---
+++ Enemy/RedTankEnemy.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Grace on 2025/5/19.
+//
+
+#include "RedTankEnemy.hpp"
+RedTankEnemy::RedTankEnemy(int x, int y) : Enemy("play/enemy-10.png", x, y, 50, 40, 50, 300){
+
+}
---
+++ Enemy/RedTankEnemy.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Grace on 2025/5/19.
+//
+
+#ifndef REDTANKENEMY_HPP
+#define REDTANKENEMY_HPP
+#include "Enemy.hpp"
+
+class RedTankEnemy : public Enemy {
+public:
+    RedTankEnemy(int x, int y);
+};
+#endif //REDTANKENEMY_HPP
